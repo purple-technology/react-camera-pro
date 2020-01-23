19b3c22db148616d7dcef2290c7c83c66d7b2d82
Martin Urban <murban@purple-technology.com>
2020-01-23 15:14:39 +0100

    Updates

---
+++ precache-manifest.1814e2564c0d7a59ac59c74e6275f290.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7778b56c1fe11f7ecabed6c530411c2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40984e28c848eac85437",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "06fb08d3d8140076ce1f",
+    "url": "/static/js/2.eed73d35.chunk.js"
+  },
+  {
+    "revision": "40984e28c848eac85437",
+    "url": "/static/js/main.26b8af00.chunk.js"
+  },
+  {
+    "revision": "84d6721e64a18fe343e6",
+    "url": "/static/js/runtime-main.f47b6839.js"
+  }
+]);
